99c8eb9cd1522cfc2f41fbf33aa3510a9fd958de
yow5110 <yuanxi.wang@gmail.com>
2022-09-26 13:55:04 -0500

    init

--- gen.py
+++ gen.py
@@ -7,7 +7,6 @@ df = [dict(Task="UGC SP #1", Start='2022-01-25', Finish='2022-01-26', Resource='
       dict(Task="Data collect for TRACDAT", Start='2022-03-01', Finish='2022-05-01', Resource='tasks'),
 
       dict(Task="Vote on PHYS pre-reqs", Start='2022-08-17', Finish='2022-08-24', Resource='tasks'),
-
       dict(Task="UGC FA #1", Start='2022-09-26', Finish='2022-09-27', Resource='regular'),
       dict(Task="UGC FA #2", Start='2022-10-31', Finish='2022-11-01', Resource='regular'),
       dict(Task="UGC FA #3", Start='2022-11-28', Finish='2022-11-29', Resource='regular'),
@@ -15,6 +14,7 @@ df = [dict(Task="UGC SP #1", Start='2022-01-25', Finish='2022-01-26', Resource='
 
       dict(Task="CUWiP", Start='2022-08-29', Finish='2022-10-10', Resource='awards'),
       dict(Task="WiP UNT Group Grant", Start='2022-10-08', Finish='2022-10-22', Resource='awards'),
+
       dict(Task="Ugrad Scholarship Noms.", Start='2023-01-25', Finish='2023-02-08', Resource='awards'),
       dict(Task="Honors Day Nominations", Start='2023-02-08', Finish='2023-02-22', Resource='awards')
      ]
@@ -26,5 +26,10 @@ colors = {'tasks': 'rgb(252,192,89)',
 fig = ff.create_gantt(df, colors=colors, index_col='Resource', show_colorbar=True,
                       group_tasks=True)
 fig.update_layout(xaxis = dict(showgrid=True, ticklabelmode="period", dtick='M1')  )
+fig.update_layout(title="<b>UGC Gantt Chart 2022</b>", 
+                font=dict(family="Arial",size=18),
+                legend=dict(orientation="h")
+                )
+
 #fig.show()
 fig.write_html('save.html')
